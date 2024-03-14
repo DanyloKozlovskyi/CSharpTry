866d3d0c35070aed29f69be7af427a0bc24b53f5
Danylo <you@example.com>
2024-03-14 22:24:15 +0200

    first commit

---
+++ prep_modul1_2/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prep_modul1_2
+{
+    public class Rectangle : IShape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        double IShape.Perimeter()
+        {
+            return 2 * (Height + Width);
+        }
+        public override string ToString()
+        {
+            return $"{Height}, {Width}";
+        }
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
